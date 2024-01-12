5e5565d7e515399d0af26502efe1f2114d7e20cc
Ruofei Yang <yangshogun@gmail.com>
2024-01-12 12:39:28 -0700

    Create password generator

---
+++ index.js
@@ -0,0 +1,29 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+// on clicking the button, generate two passwords
+// each password should be 15 characters long
+
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
+let button = document.getElementById("generate-pw-btn")
+let passwordLength = 15
+let newPassword = ""
+
+button.addEventListener("click", generatePasswords)
+
+
+function passwordGenerator() {
+    let password = "" // Initialize password as an empty string
+    for (i = 0; i < passwordLength; i++) {
+        let randomCharacter = Math.floor(Math.random()*characters.length)
+        password += characters[randomCharacter]
+    }
+    
+    return password
+}
+
+function generatePasswords() {
+    passwordOne.textContent = passwordGenerator()
+    passwordTwo.textContent = passwordGenerator()
+}
